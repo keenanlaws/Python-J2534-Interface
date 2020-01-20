1df0e2c651d4d8ab21932fd05e01639ea402c426
ecuunlock <engineering@ecuunlock.com>
2020-01-20 08:35:11 -0500

    Update J2534_Library.py

--- J2534_Library.py
+++ J2534_Library.py
@@ -1,4 +1,4 @@
- 
+# * 
 # * Copyright (c) 2020, ecuunlock
 # *
 # * EMAIL = engineering@ecuunlock.com
@@ -28,7 +28,7 @@
 # * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 # * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+# *
 import winreg
 import ctypes
 import os
