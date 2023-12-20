7795a38bc1c2034bb9d09f29148a5019a2df09de
ecuunlock <engineering@ecuunlock.com>
2023-12-20 14:09:01 -0500

    Update __init__.py

--- J2534/__init__.py
+++ J2534/__init__.py
@@ -1,6 +1,6 @@
 from .Define import ProtocolID, BaudRate, TxFlags, FilterType, IoctlID, BaudRate, Parameter, Flags
-from .wrapper import J2534Api, japi
-from .wrapper import PassThruMsgBuilder, PassThruMskMsg, PassThruPatternMsg, PassThruFlowControlMsg
+from .wrapper import J2534Api, j2534_api
+from .wrapper import PassThruMsgBuilder, PassThruMsg
 from .wrapper import pt_connect, pt_disconnect
 from .wrapper import pt_open, pt_close
 from .wrapper import pt_read_message, pt_write_message
@@ -9,5 +9,5 @@ from .wrapper import pt_start_message_filter, pt_stop_message_filter, pt_start_e
 from .wrapper import pt_start_periodic_message, pt_stop_periodic_message
 from .wrapper import read_battery_volts, clear_transmit_buffer, clear_receive_buffer, clear_periodic_messages
 
-getDevices = japi.get_devices
-setDevice = japi.set_device
+get_list_j2534_devices = j2534_api.get_devices
+set_j2534_device_to_connect = j2534_api.set_device
